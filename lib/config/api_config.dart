041f403ecdafc47e48c070cae2f624f796e90b9d
fajarlm <kusumafajar080802@gmail.com>
2026-07-03 21:24:09 +0700

    chore: update base URL to production URL

--- lib/config/api_config.dart
+++ lib/config/api_config.dart
@@ -2,11 +2,12 @@ import 'package:flutter/foundation.dart';
 
 class ApiConfig {
   static String get baseUrl {
-    if (kIsWeb) {
-      return 'http://localhost:3001';
-    } else {
-      return 'http://192.168.1.37:3001';
-    }
+    // if (kIsWeb) {
+    //   return 'http://localhost:3001';
+    // } else {
+    //   return 'http://192.168.1.37:3001';
+    // }
+    return 'https://gotoh-be-express.vercel.app';
   }
 
   // Endpoint uploads untuk gambar
